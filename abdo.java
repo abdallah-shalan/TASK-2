53953346c630e6c1196d8d419d8142c4f9a31c62
shalan2002 <bdallahshalan@gmail.com>
2023-12-14 12:04:10 +0200

    Added code files

---
+++ abdo.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication;
+
+
+/**
+ *
+ * @author SHA3LAN
+ */
+public class abdo {
+    
+    public static void main(String[] args) {
+       System.out.println("Abdo");
+    }
+}    
+        
+     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
